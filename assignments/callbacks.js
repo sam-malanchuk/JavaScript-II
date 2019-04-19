97aa987274c3f6289b81202058293a0bb5962bff
sam-malanchuk <malanchuksam@gmail.com>
2019-04-19 17:34:55 -0400

    re-committing to push

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,7 +24,6 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
