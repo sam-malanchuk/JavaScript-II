822e00d6cc99884d522f36047d54a854a3ddc48c
sam-malanchuk <malanchuksam@gmail.com>
2019-04-18 21:00:38 -0400

    completed challenge in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function stillFirstLayerScope() {
+  firstLayerScope(); // I can call this function because it's not above the lexical scope
+}
+
+function thirdOnFirstScope() {
+  console.log("I'm being called from three scopes deep")
+}
+
+function firstLayerScope() {
+  function secondLayerScope() {
+      function thirdLayerScope() {
+        thirdOnFirstScope(); // even though this is not on the same lexical scope I can run it because it's outside of the current scope
+        console.log("third scope layer console logging");
+      }
+    console.log("second scope layer console logging");
+    thirdLayerScope(); // running the third layer scope because it's in the lexical scope
+  } // 2st scope from the outside
+  console.log("first scope layer console logging");
+  secondLayerScope(); // able to run function because within it's lexical scope
+} // 1st scope from the outside
+
+stillFirstLayerScope();
+// secondLayerScope(); // I cannot run this function because it's not on the current lexical scope, see line 22
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
